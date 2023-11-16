dc514b6b64dcadd95214bfef1fb2cf07a18f47a2
agrevster <138630000+agrevster@users.noreply.github.com>
2023-11-16 14:10:02 -0600

    Added a new util for getting input files as a raw string rather than a list

--- src/Utils.kt
+++ src/Utils.kt
@@ -3,10 +3,16 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
- * Reads lines from the given input txt file.
+ * Reads lines from the given input txt file and returns them as a list of strings with each entry as a new line.
  */
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
+
+/**
+ * Reads lines from the given input txt file and returns them as a single string.
+ */
+fun readRawInput(name: String) = File("src", "$name.txt")
+    .readLines().joinToString(separator = "\n")
 
 /**
  * Converts string to md5 hash.
