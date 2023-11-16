e60b0aebd9c12c7c42d9b0874b96917b06be0970
agrevster <138630000+agrevster@users.noreply.github.com>
2023-11-16 14:33:57 -0600

    Day 1!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    data class Elf(val foodItems: List<Int>){
+        val totalCalories = foodItems.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+   fun getElves(input: String): List<Elf> {
+       val elves = mutableListOf<Elf>()
+
+       input.split("\n\n").forEach {elf ->
+           elves += Elf(elf.lines().map { it.toInt() })
+       }
+       return elves
+   }
+
+    fun part1(input: String): Int {
+        return getElves(input).maxOf { it.totalCalories }
+    }
+
+    fun part2(input: String): Int {
+        return getElves(input).map { it.totalCalories }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readRawInput("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readRawInput("Day01")
     part1(input).println()
     part2(input).println()
 }
